56acffbc3704d75db65add78997058a3413b2e24
Maxiboo <maxime.gourdon@gmail.com>
2017-10-30 15:21:09 +0100

    added rules of tic tac toe in readme

---
+++ src/app/table-board/table-board.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-board',
+  templateUrl: './table-board.component.html',
+  styleUrls: ['./table-board.component.css']
+})
+export class TableBoardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
